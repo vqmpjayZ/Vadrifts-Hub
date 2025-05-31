1c010546d1f386cb04a8de76aaa41a872fdca32d
vqmpjay <bakir.butkovic@osgrbavica2.edu.ba>
2025-05-31 21:48:17 +0200

    Create Tiny-Wheels.lua

---
+++ Games/Tiny-Wheels/Tiny-Wheels.lua
@@ -0,0 +1,10 @@
+--[[
+ __   __   ______     _____     ______     __     ______   ______   ______    
+/\ \ / /  /\  __ \   /\  __-.  /\  == \   /\ \   /\  ___\ /\__  _\ /\  ___\   
+\ \ \'/   \ \  __ \  \ \ \/\ \ \ \  __<   \ \ \  \ \  __\ \/_/\ \/ \ \___  \  
+ \ \__|    \ \_\ \_\  \ \____-  \ \_\ \_\  \ \_\  \ \_\      \ \_\  \/\_____\ 
+  \/_/      \/_/\/_/   \/____/   \/_/ /_/   \/_/   \/_/       \/_/   \/_____/ 
+                           dsc.gg/vadriftz (in browsers)
+]]
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/vqmpjayZ/Vadrifts-Hub/refs/heads/main/Games/Tiny-Wheels/source.lua"))()
